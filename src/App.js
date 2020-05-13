b44d6f41516bffad7c61475e42e7c6b34ccdb323
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-12 23:19:58 -0500

    add Pet js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom"; //use {} if you want to import one specific feature
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
@@ -31,7 +27,7 @@ const App = () => {
   );
 };
 
-ReactDOM.render(
+render(
   //render overrides anything that was hard coded in that html area
   React.createElement(App),
   document.getElementById("root")
