9579b7fc47e3adb5a77994e02c2adc365b64f278
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-05-13 01:17:22 -0500

    finished custom hook and hooks lesson

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   //const [currentVariable, updatesCurrentVariable] e updates useState
@@ -7,9 +8,9 @@ const SearchParams = () => {
   //hooks NEVER go inside if statements or for loops or conditional or unpredictable logic
   //hooks must stay in order
   const [location, setLocation] = useState("Seattle, WA"); //defaultState before update
-  const [animal, setAnimal] = useState("dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
@@ -23,39 +24,8 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)} //e is variable for event
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>All</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => setBreed.target.value}
-            onBlur={(e) => setBreed.target.value}
-            disabled={!breeds.length} // or breeds.length === 0
-          >
-            <option>All</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
